fa37e53270bd6205f1ea195ede8720037f19f9aa
KarolJaskolka <jaskolka.karol@gmail.com>
2022-08-28 17:54:21 +0200

    Install vue-fontawesome and import icons

---
+++ src/icons.ts
@@ -0,0 +1,62 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faAngular,
+  faCss3Alt,
+  faGitAlt,
+  faGithub,
+  faHtml5,
+  faJenkins,
+  faJira,
+  faJs,
+  faLinkedin,
+  faNodeJs,
+  faNpm,
+  faPython,
+  faReact,
+  faSass,
+  faUbuntu,
+  faUnity,
+  faVuejs,
+  faYarn,
+} from "@fortawesome/free-brands-svg-icons";
+
+import {
+  faBriefcase,
+  faChevronUp,
+  faCode,
+  faCube,
+  faDatabase,
+  faEnvelope,
+  faGraduationCap,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faAngular,
+  faBriefcase,
+  faChevronUp,
+  faCode,
+  faCube,
+  faCss3Alt,
+  faDatabase,
+  faEnvelope,
+  faGitAlt,
+  faGithub,
+  faGraduationCap,
+  faHtml5,
+  faJenkins,
+  faJira,
+  faJs,
+  faLinkedin,
+  faNodeJs,
+  faNpm,
+  faPython,
+  faReact,
+  faSass,
+  faUbuntu,
+  faUser,
+  faUnity,
+  faVuejs,
+  faYarn
+);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import i18n from "./i18n";
 
-createApp(App).use(i18n).mount("#app");
+import "@/styles/index.scss";
+import "@/icons";
+
+createApp(App)
+  .use(i18n)
+  .component("FontAwesomeIcon", FontAwesomeIcon)
+  .mount("#app");
